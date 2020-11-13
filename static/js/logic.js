28fc10db4d78b3b8e7db77df52782e9ba659d863
Ken <[KENJEMA@GMAIL.COM]>
2020-11-13 03:09:50 -0600

    6

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,8 @@
 //LayerGroup for Earthquakes
 let earthquakes = new L.LayerGroup();
 
+//Variables to Reference Layers
+let OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 16,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
